0ed63ca0c48c858358eb6d054a9373be60a35286
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-16 15:55:46 +0600

    constans actions reducers added

--- src/redux/counterSlice.js
+++ src/redux/counterSlice.js
@@ -1,24 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: {
-    value: 0,
-  },
-  reducers: {
-    increment: (state) => {
-      state.value = state.value + 1;
-    },
-    decrement: (state) => {
-      state.value = state.value - 1;
-    },
-    reset: (state) => {
-        state.value = 0;
-      },
+// const counterSlice = createSlice({
+//   name: "counter",
+//   initialState: {
+//     value: 0,
+//   },
+//   reducers: {
+//     increment: (state) => {
+//       state.value = state.value + 1;
+//     },
+//     decrement: (state) => {
+//       state.value = state.value - 1;
+//     },
+//     reset: (state) => {
+//         state.value = 0;
+//       },
 
-  },
-});
+//   },
+// });
 
-export const { increment, decrement,reset } = counterSlice.actions;
+// export const { increment, decrement,reset } = counterSlice.actions;
 
-export default counterSlice.reducer;
+// export default counterSlice.reducer;
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "../counterSlice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import counterSlice from "../counterSlice";
 
-export default configureStore({
-  reducer: {
-    counter: counterSlice,
-  },
-});
+// export default configureStore({
+//   reducer: {
+//     counter: counterSlice,
+//   },
+// });
+import { createStore } from "redux";
+import counterReducer from "../reducer/counterReducer";
+const store = createStore(counterReducer);
+
+export default store;
